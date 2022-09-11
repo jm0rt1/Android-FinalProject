06eaa0e345bf66ebc51974665db1ad8daa220f33
Jamie Mortensen <16mortensenj@gmail.com>
2022-09-11 14:22:45 -0400

    Able to decode html escape sequences

--- app/src/main/java/com/example/finalproject/CurrentGameActivity.java
+++ app/src/main/java/com/example/finalproject/CurrentGameActivity.java
@@ -1,8 +1,8 @@
 package com.example.finalproject;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.example.finalproject.model.Model;
@@ -10,6 +10,8 @@ import com.example.finalproject.model.Model;
 public class CurrentGameActivity extends AppCompatActivity {
     int currentQuestion= 0;
     TextView categoryTextView;
+    TextView questionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +19,8 @@ public class CurrentGameActivity extends AppCompatActivity {
         Model m = Model.getInstance();
         categoryTextView = findViewById(R.id.category_text_view);
         categoryTextView.setText(Model.getInstance().getCurrentGame().getCategory().getmName());
-
-
+        questionTextView = findViewById(R.id.question_text_view);
+        questionTextView.setText(Html.fromHtml(Model.getInstance().getCurrentGame().getQuestion(currentQuestion).getQuestion()).toString());
 
     }
 
--- app/src/main/java/com/example/finalproject/model/Game.java
+++ app/src/main/java/com/example/finalproject/model/Game.java
@@ -6,7 +6,9 @@ public class Game {
     public Game(Categories.Category category){
         mStatItem = new Stats.StatItem(category);
     }
-
+    public Questions.Question getQuestion(int i){
+        return mQuestions.getQuestions().get(i);
+    }
     public Categories.Category getCategory(){
         return mStatItem.mCategory;
     }
