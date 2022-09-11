22de2fca52e7431991168019069ea9673873179b
Jamie Mortensen <16mortensenj@gmail.com>
2022-09-11 12:47:38 -0400

    beginning work on other activities

--- app/src/main/java/com/example/finalproject/CurrentGameActivity.java
+++ app/src/main/java/com/example/finalproject/CurrentGameActivity.java
@@ -10,5 +10,9 @@ public class CurrentGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_game);
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/finalproject/viewHolders/GameCategoryItemViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.finalproject.viewHolders;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GameCategoryItemViewHolder extends RecyclerView.ViewHolder{
+
+
+    public GameCategoryItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+    }
+}
