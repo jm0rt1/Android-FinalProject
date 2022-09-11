7008f8a7a9d4f63a64ae290ab3bdd035539ad286
Jamie Mortensen <16mortensenj@gmail.com>
2022-09-11 11:57:55 -0400

    moved stats items

--- app/src/main/java/com/example/finalproject/model/Stats.java
+++ app/src/main/java/com/example/finalproject/model/Stats.java
@@ -5,6 +5,56 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 
 public class Stats {
+
+    public static class StatItem {
+        Categories.Category mCategory;
+        int mAnswered = 0 ;
+        int mCorrect = 0 ;
+        double mPercentCorrect = 0.0;
+
+        public StatItem(Categories.Category category){
+            mCategory = category;
+        }
+        public void addResult(StatItem resultToAdd) throws Exception {
+            if (mCategory != resultToAdd.mCategory){
+                throw new Exception("cannot add result to differing categories");
+            }
+            mAnswered += resultToAdd.mAnswered;
+            mCorrect += resultToAdd.mCorrect;
+            double perc = (double)mCorrect/(double)mAnswered;
+            mPercentCorrect = (double)Math.round(perc*10000)/100;
+
+        }
+
+        public Categories.Category getCategory() {
+            return mCategory;
+        }
+
+        public int getAnswered() {
+            return mAnswered;
+        }
+
+        public int getCorrect() {
+            return mCorrect;
+        }
+
+        public double getPercentCorrect() {
+            return mPercentCorrect;
+        }
+
+        public void addAnswered(boolean correct){
+            mAnswered ++;
+            if (correct){
+                mCorrect ++;
+
+            }
+            double perc = (double)mCorrect/(double)mAnswered;
+            mPercentCorrect = (double)Math.round(perc*10000)/100;
+        }
+
+
+    }
+
     ArrayList<StatItem> mCategoryStats = new ArrayList<>();
 
 
