4a0a313fba89925ec7dec07af9ee71690f6432c8
Jamie Mortensen <16mortensenj@gmail.com>
2022-09-11 11:58:36 -0400

    created the model

---
+++ app/src/main/java/com/example/finalproject/model/Model.java
@@ -0,0 +1,24 @@
+package com.example.finalproject.model;
+
+public class Model {
+    private static Model instance;
+    private boolean isInitialized = false;
+
+
+    private Categories categories;
+    private Stats stats;
+
+    public static synchronized Model getInstance() {
+        if (instance == null) {
+            instance = new Model();
+        }
+        return instance;
+    }
+
+    public void loadModels() {
+        if (!isInitialized) {
+
+            isInitialized = true;
+        }
+    }
+}
